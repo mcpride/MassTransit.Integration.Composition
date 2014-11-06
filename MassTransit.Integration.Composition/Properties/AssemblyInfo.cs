63c927f87d09176625e68afa1f300f9184018f8f
mcpride <mcpride@gmx.de>
2014-11-06 13:43:56 +0100

    version incremented to 1.0.2.0

--- MassTransit.Integration.Composition/Properties/AssemblyInfo.cs
+++ MassTransit.Integration.Composition/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("3f6ee35f-17c4-4be9-9180-80985dc083da")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
